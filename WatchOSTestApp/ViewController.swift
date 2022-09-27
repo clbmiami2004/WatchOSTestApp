5f5b17b18ee806bfc5a67929d218d451e0aa5ce0
Christian Lorenzo <lorenzobchristian@gmail.com>
2022-09-27 17:12:42 -0500

    Initial Commit

---
+++ WatchOSTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchOSTestApp
+//
+//  Created by Christian Lorenzo on 9/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
