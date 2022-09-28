c1d23da3fcaf806464608882f293f60d1d43a73b
Christian Lorenzo <lorenzobchristian@gmail.com>
2022-09-28 15:51:47 -0500

    Update ViewController.swift
    Initialized the WCSession and added delegates to the Class

--- WatchOSTestApp/ViewController.swift
+++ WatchOSTestApp/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import WatchConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WCSessionDelegate {
 
+    var wcSession: WCSession!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        wcSession = WCSession.default
+        wcSession.delegate = self
+        wcSession.activate()
     }
-
+    
+    //WCSession functions:
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        <#code#>
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        <#code#>
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        <#code#>
+    }
 
 }
 
